191aa7885a9fc6c00a869d0e5a0bc0308c923e3d
Chiraz588 <75127710+Chiraz588@users.noreply.github.com>
2021-11-26 12:48:29 +0100

    first commit

---
+++ src/components/MoviesList/Movieslist.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Moviecard from "../movieCard/MovieCard";
+import "./movieslist.css";
+
+const Movielist = ({ movies, textFilter, starsRate }) => {
+  return (
+    <div className="movies">
+      {movies
+        .filter(
+          (movie) =>
+            movie.rate >= starsRate &&
+            movie.title.toLowerCase().includes(textFilter.toLowerCase())
+        )
+        .map((movie) => (
+          <Link
+          exact
+            to={`/MovieTrailer/${movie.id}`}
+            style={{ textDecoration: "none" }}
+          >
+            {" "}
+            <Moviecard movie={movie} key={movie.id} />{" "}
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default Movielist;
